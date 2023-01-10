bb20cd60e666ae878501aa7ee44ae11ea98e4aa6
Jeremy Maia <jmaia@student.42.fr>
2023-01-10 19:33:21 +0100

    Send message through webSocket

--- front/src/chat/UserInput.tsx
+++ front/src/chat/UserInput.tsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import useWebSocket from 'react-use-websocket';
+import IMessage from './IMessage';
+import { ws_url as WS_URL } from '../config.json';
+
 export default function UserInput() {
+    const [userInput, setUserInput] = useState('');
+    const { sendMessage } = useWebSocket(WS_URL, {
+        share: true,
+    });
     return (
-        <form>
-            <input type="text" />
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                value={userInput}
+                onChange={(event) => setUserInput(event.target.value)}
+            />
             <input type="submit" value="Envoyer" />
         </form>
     );
+    function onSubmit(e: any) {
+        e.preventDefault();
+        const message: IMessage = { user: 'Alice', message: userInput };
+        const jsonMessage = { event: 'chat', data: message };
+        sendMessage(JSON.stringify(jsonMessage));
+        setUserInput('');
+    }
 }
