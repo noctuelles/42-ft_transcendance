91bc9ab09956f27c2412ec78d85f446ce76eaf71
noctuelles <plouvel@student.42.fr>
2023-01-11 16:59:44 +0100

    updated navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,6 @@ import InfoBox from './components/global/InfoBox';
 import AppRouter from './components/router/AppRouter';
 import InfoBoxContextProvider from './context/InfoBoxContext';
 import UserContextProvider from './context/UserContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
