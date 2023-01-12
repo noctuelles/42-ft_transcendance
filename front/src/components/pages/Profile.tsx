8a401d6ece28de46fd33380a8c434d87176163b9
noctuelles <plouvel@student.42.fr>
2023-01-12 15:08:58 +0100

    draft layout of profile page

--- front/src/components/pages/Profile.tsx
+++ front/src/components/pages/Profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../../style/Profile.css'
+import {UserContext} from '../../context/UserContext';
 
 class Profile extends React.Component<{}, {
 		photo: any,
@@ -9,6 +10,7 @@ class Profile extends React.Component<{}, {
 	}> {
 	constructor(props: any) {
 		super(props);
+
 
 		this.state = {
 			photo: null,
@@ -20,8 +22,17 @@ class Profile extends React.Component<{}, {
 
 	render() {
 		return (
-			<div className="profilePage">
-				<p>blabla</p>
+			<div className="container">
+				<div className="top-header">
+					<p>{this.state.username}</p>
+					<div className="search">
+						<input type="text" placeholder="Search a profile..."/>
+						<button type="submit">bonjour</button>
+					</div>
+					<img src="https://cdn.intra.42.fr/users/1022f4b45a249d0c6cea0572d68baab8/plouvel.jpg"/>
+				</div>
+				<hr></hr>
+				<p>Lorem ipsum</p>
 			</div>
 		);
 	}
