ffd33b69bfbf6e75e574cc83545750f98d5783a2
dhubleur <dhubleur@student.42.fr>
2023-01-12 16:01:11 +0100

    fix(USER): Add profile_picture to user

--- back/src/modules/auth/services/auth.service.ts
+++ back/src/modules/auth/services/auth.service.ts
@@ -113,6 +113,7 @@ export class AuthService {
             user: {
                 id: user.id,
                 name: user.name,
+                profile_picture: user.profile_picture,
             },
         });
     }
