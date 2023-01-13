61428fa606d56ece48bec2e38592cb49c39f5459
Jeremy Maia <jmaia@student.42.fr>
2023-01-13 17:29:57 +0100

    Add chat page to NavBar

--- front/src/components/global/NavBar.tsx
+++ front/src/components/global/NavBar.tsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { Link }from 'react-router-dom'
-import '../../style/NavBar.css'
-import {UserContext} from '../../context/UserContext';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../style/NavBar.css';
+import { UserContext } from '../../context/UserContext';
 
 function NavBar(props: any) {
-	const userContext = React.useContext(UserContext);
+    const userContext = React.useContext(UserContext);
 
-  return (
-		<nav className="navBar">
-			<ul className="navLinks">
-				<li className="navItem"><Link to="/">Play</Link></li>
-				<li className="navItem"><Link to="/social">Social</Link></li>
-			</ul>
-			<div className="navRight">
-				<img src="wewe"/>
-				<Link to="/profile">{userContext.name}</Link>
-			</div>
-		</nav>
-  );
+    return (
+        <nav className="navBar">
+            <ul className="navLinks">
+                <li className="navItem">
+                    <Link to="/">Play</Link>
+                </li>
+                <li className="navItem">
+                    <Link to="/social">Social</Link>
+                </li>
+                <li className="navItem">
+                    <Link to="/chat">Chat</Link>
+                </li>
+            </ul>
+            <div className="navRight">
+                <img src="wewe" />
+                <Link to="/profile">{userContext.name}</Link>
+            </div>
+        </nav>
+    );
 }
 
 export default NavBar;
