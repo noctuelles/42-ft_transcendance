27b3eb5be694c481b174888c7cf75d9c447c4b45
Jeremy Maia <jmaia@student.42.fr>
2023-01-13 13:53:08 +0100

    Integrate internal functions of Messages into it

--- front/src/components/chat/Messages.tsx
+++ front/src/components/chat/Messages.tsx
@@ -12,38 +12,38 @@ export default function Messages() {
             ))}
         </div>
     );
-}
 
-function getMessages(): IMessage[] {
-    const messages = useRef<IMessage[]>([]);
-    useWebSocket(WS_URL, {
-        share: true,
-        onMessage: ({ data }: { data?: string }) => {
-            if (!data || !isChatMessage(data)) {
-                return;
-            }
-            const newMessage = parseMessage(data);
-            messages.current = [...messages.current, newMessage];
-        },
-        filter: ({ data }: { data: string }) => {
-            return isChatMessage(data);
-        },
-    });
-    return messages.current;
-}
-
-function isChatMessage(rawMessage: string): boolean {
-    try {
-        var message = JSON.parse(rawMessage);
-    } catch (error) {
-        return false;
+    function getMessages(): IMessage[] {
+        const messages = useRef<IMessage[]>([]);
+        useWebSocket(WS_URL, {
+            share: true,
+            onMessage: ({ data }: { data?: string }) => {
+                if (!data || !isChatMessage(data)) {
+                    return;
+                }
+                const newMessage = parseMessage(data);
+                messages.current = [...messages.current, newMessage];
+            },
+            filter: ({ data }: { data: string }) => {
+                return isChatMessage(data);
+            },
+        });
+        return messages.current;
     }
-    return message?.['event'] == 'chat';
-}
 
-function parseMessage(rawMessage: string): IMessage {
-    const jsonMessage = JSON.parse(rawMessage);
-    return jsonMessage['data'];
+    function isChatMessage(rawMessage: string): boolean {
+        try {
+            var message = JSON.parse(rawMessage);
+        } catch (error) {
+            return false;
+        }
+        return message?.['event'] == 'chat';
+    }
+
+    function parseMessage(rawMessage: string): IMessage {
+        const jsonMessage = JSON.parse(rawMessage);
+        return jsonMessage['data'];
+    }
 }
 
 function Message(props: IMessage) {
