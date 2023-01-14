8e6e83ccb10620797c9c1f0287a7a4beb62e5260
dhubleur <dhubleur@student.42.fr>
2023-01-14 14:31:25 +0100

    style(BACK): Use tab indentation

--- back/src/modules/websockets/websockets.gateway.ts
+++ back/src/modules/websockets/websockets.gateway.ts
@@ -1,25 +1,25 @@
 import {
-    OnGatewayConnection,
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
+	OnGatewayConnection,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
 } from '@nestjs/websockets';
 import { Server } from 'http';
 import { WebsocketsService } from './websockets.service';
 
 @WebSocketGateway()
 export class WebsocketGateway implements OnGatewayConnection {
-    constructor(private readonly websocketsService: WebsocketsService) {}
+	constructor(private readonly websocketsService: WebsocketsService) {}
 
-    @WebSocketServer() server: Server;
+	@WebSocketServer() server: Server;
 
-    async afterInit(server: Server) {
-        server.on('connection', (socket, request) => {
-            socket['request'] = request;
-        });
-    }
+	async afterInit(server: Server) {
+		server.on('connection', (socket, request) => {
+			socket['request'] = request;
+		});
+	}
 
-    async handleConnection(socket) {
-        await this.websocketsService.registertSocket(socket);
-    }
+	async handleConnection(socket) {
+		await this.websocketsService.registertSocket(socket);
+	}
 }
