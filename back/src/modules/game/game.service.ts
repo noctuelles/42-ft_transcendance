8960ab0d9fca6db8748105f60645a24089295ccd
dhubleur <dhubleur@student.42.fr>
2023-01-17 15:00:39 +0100

    style(GAME): Remove unused console log

--- back/src/modules/game/game.service.ts
+++ back/src/modules/game/game.service.ts
@@ -8,7 +8,6 @@ export class GameService {
 	constructor(private readonly websocketsService: WebsocketsService) {}
 
 	joinQueue(socket) {
-		console.log(socket.user.name + ' joined queue');
 		this.queue.push(socket);
 		if (this.queue.length >= 2) {
 			const player1 = this.queue.shift();
@@ -24,12 +23,6 @@ export class GameService {
 					profile_picture: player2.user.profile_picture,
 				},
 			};
-			console.log(
-				'Match found: ' +
-					player1.user.name +
-					' vs ' +
-					player2.user.name,
-			);
 			this.websocketsService.send(player1, 'matchmaking', msg);
 			this.websocketsService.send(player2, 'matchmaking', msg);
 		}
@@ -37,7 +30,6 @@ export class GameService {
 
 	cancelQueue(socket) {
 		if (this.queue.includes(socket.user)) {
-			console.log(socket.user.name + ' left queue');
 			this.queue.splice(this.queue.indexOf(socket), 1);
 		}
 	}
