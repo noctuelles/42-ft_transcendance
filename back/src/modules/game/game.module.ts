1b916cbb6ada882f4e9bd223ee84c03a50ecd7e6
dhubleur <dhubleur@student.42.fr>
2023-01-23 13:25:00 +0100

    fix(GAME): Update for new db schema

--- back/src/modules/game/game.module.ts
+++ back/src/modules/game/game.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
 import { WebsocketsModule } from '../websockets/websockets.module';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
 
 @Module({
-	imports: [UsersModule, WebsocketsModule],
+	imports: [UsersModule, WebsocketsModule, PrismaModule],
 	controllers: [],
 	providers: [GameGateway, GameService],
 	exports: [],
