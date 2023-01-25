ab33b12303cb739536c15591b2a1d665c7c7ebcc
dhubleur <dhubleur@student.42.fr>
2023-01-25 14:01:01 +0100

    refactor(CDN): Remove unused userService

--- back/src/modules/cdn/cdn.controller.ts
+++ back/src/modules/cdn/cdn.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
 const fs = require('fs');
 
 @Controller('cdn')
 export class CdnController {
-	constructor(private readonly userService: UsersService) {}
-
 	@Get('user/:userLogin')
 	async user(@Param('userLogin') userLogin: string, @Res() res) {
 		const imgPath = `./cdn/profile_pictures/${userLogin}`;
