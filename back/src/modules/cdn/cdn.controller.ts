440e612ae90d12c7c92ff5eeb181dcf906cc8f85
dhubleur <dhubleur@student.42.fr>
2023-01-25 14:00:35 +0100

    fix(CDN): Provide a default file if doesn't exist
    Closes #17

--- back/src/modules/cdn/cdn.controller.ts
+++ back/src/modules/cdn/cdn.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+const fs = require('fs');
 
 @Controller('cdn')
 export class CdnController {
@@ -8,6 +9,12 @@ export class CdnController {
 	@Get('user/:userLogin')
 	async user(@Param('userLogin') userLogin: string, @Res() res) {
 		const imgPath = `./cdn/profile_pictures/${userLogin}`;
-		return res.sendFile(imgPath, { root: 'public' });
+		if (fs.existsSync(imgPath)) {
+			return res.sendFile(imgPath, { root: 'public' });
+		} else {
+			return res.sendFile('default.jpg', {
+				root: 'public/cdn/profile_pictures',
+			});
+		}
 	}
 }
