62f03d614202ed7647bb81221c57698eaad47ce4
Paul Louvel <plouvel@student.42.fr>
2023-02-05 16:14:58 +0100

    chat layout

--- front/src/components/pages/Chat.tsx
+++ front/src/components/pages/Chat.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Button from '../global/Button';
 import ChannelCreationForm from './details/channel/ChannelCreationForm';
 import ChannelList from './details/channel/ChannelList';
+import Message from './details/channel/Message';
 
 export default function Chat() {
 	const [showCreationForm, setShowCreationForm] = useState(false);
@@ -20,7 +21,40 @@ export default function Chat() {
 				<hr />
 				<ChannelList />
 			</div>
-			<div className="chat-page-center"></div>
+			<div className="chat-page-center">
+				<ol className="chat-container">
+					<Message
+						from="plouvel"
+						self={false}
+						content="Bonjour à tous"
+					/>
+					<Message
+						from="plouvel"
+						self={false}
+						content="Bonjour à tous"
+					/>
+					<Message
+						from="jmaia"
+						self={true}
+						content="Bonjour à tous"
+					/>
+					<Message
+						from="plouvel"
+						self={false}
+						content="Bonjour à tous"
+					/>
+					<Message
+						from="jmaia"
+						self={true}
+						content="Bonjour à tous ceci est un emssage parfaitement long"
+					/>
+				</ol>
+				<input
+					className="textarea"
+					type="text"
+					placeholder="Type your message here..."
+				/>
+			</div>
 			<div className="chat-page-right-side"></div>
 		</div>
 	) : (
--- front/src/components/pages/details/channel/ChannelList.tsx
+++ front/src/components/pages/details/channel/ChannelList.tsx
@@ -1,7 +1,16 @@
 import '@/style/details/chat/ChannelList.css';
+import Channel from './Channel';
 
 // s'occupe de recuperer les channels (voir le code de Jeremy)
 
+const chan = [{ name: 'Le channel' }, { name: 'Le Channel' }];
+
 export default function ChannelList() {
-	return <div className="channel-list"></div>;
+	return (
+		<ul className="channel-list">
+			{chan.map((c) => (
+				<Channel key={c.name} name={c.name} />
+			))}
+		</ul>
+	);
 }
