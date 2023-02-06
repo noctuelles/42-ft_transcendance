6676c5a2a5b61bb2972151ac625d6ab418c0df0c
Paul Louvel <plouvel@student.42.fr>
2023-02-06 01:03:41 +0100

    better customizable nav

--- front/src/components/global/NavBar.tsx
+++ front/src/components/global/NavBar.tsx
@@ -116,11 +116,21 @@ function NavBar(props: any) {
 			</ul>
 			<div className="navRight">
 				<img src={userContext.user.profile_picture} />
-				<Link to={`/profile/${userContext.user.name}`}>
-					{userContext.user.name}
+				<Link
+					className="nav-profile"
+					to={`/profile/${userContext.user.name}`}
+				>
+					<span>{userContext.user.name.toUpperCase()}</span>
 				</Link>
 				<button className="nav-logout" onClick={userContext.logout}>
-					Logout
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="4O"
+						height="40"
+						viewBox="0 0 512 512"
+					>
+						<path d="M160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96C43 32 0 75 0 128V384c0 53 43 96 96 96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H96c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32h64zM504.5 273.4c4.8-4.5 7.5-10.8 7.5-17.4s-2.7-12.9-7.5-17.4l-144-136c-7-6.6-17.2-8.4-26-4.6s-14.5 12.5-14.5 22v72H192c-17.7 0-32 14.3-32 32l0 64c0 17.7 14.3 32 32 32H320v72c0 9.6 5.7 18.2 14.5 22s19 2 26-4.6l144-136z" />
+					</svg>
 				</button>
 			</div>
 		</nav>
