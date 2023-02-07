fcae4c8c4a9a1e4d024163381d14e14bfb8ab7a9
dhubleur <dhubleur@student.42.fr>
2023-02-07 13:31:03 +0100

    feat(PROFILE): Join game as sepctator with button

--- front/src/components/pages/details/profile/ProfileHeader.tsx
+++ front/src/components/pages/details/profile/ProfileHeader.tsx
@@ -5,7 +5,9 @@ import getLevelByXP from './Utils';
 import { UserStatus } from './ProfileTypes';
 import { ws_url as WS_URL } from '@/config.json';
 import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { InfoBoxContext, InfoType } from '@/context/InfoBoxContext';
 
 interface ProfileHeaderProps {
 	userId: number;
@@ -19,12 +21,18 @@ interface ProfileHeaderProps {
 const ProfileHeader = (props: ProfileHeaderProps) => {
 	let [level, percentCompleted] = getLevelByXP(props.total_xp);
 	const [status, setStatus] = useState(props.status);
+	const navigate = useNavigate();
+	const infoBoxContext = useContext(InfoBoxContext);
 
 	function isStatusChangedEvent(data: any): boolean {
 		return data.event === 'user-status';
 	}
 
-	useWebSocket(WS_URL, {
+	function isSpectateMatchEvent(data: any): boolean {
+		return data.event === 'spectate-match';
+	}
+
+	const { sendMessage } = useWebSocket(WS_URL, {
 		share: true,
 		onMessage: (event) => {
 			const data = JSON.parse(event.data);
@@ -32,12 +40,35 @@ const ProfileHeader = (props: ProfileHeaderProps) => {
 				if (data.data.id === props.userId) {
 					setStatus(data.data.status);
 				}
+			} else if (isSpectateMatchEvent(data)) {
+				if (data.data.status == 'error') {
+					infoBoxContext.addInfo({
+						type: InfoType.ERROR,
+						message: data.data.error,
+					});
+				} else if (data.data.status == 'success') {
+					navigate(`/play?spectate`);
+				}
 			}
 		},
 		filter: ({ data }) => {
-			return isStatusChangedEvent(JSON.parse(data));
+			return (
+				isStatusChangedEvent(JSON.parse(data)) ||
+				isSpectateMatchEvent(JSON.parse(data))
+			);
 		},
 	});
+
+	function spectateMatch() {
+		sendMessage(
+			JSON.stringify({
+				event: 'spectate-match',
+				data: {
+					id: props.userId,
+				},
+			}),
+		);
+	}
 
 	return (
 		<div className="profile-header">
@@ -47,6 +78,14 @@ const ProfileHeader = (props: ProfileHeaderProps) => {
 					<div className={`profile-status status-${status}`}>
 						{status}
 					</div>
+					{status == UserStatus.PLAYING && (
+						<button
+							className="profile-spectate-btn"
+							onClick={spectateMatch}
+						>
+							Spectate
+						</button>
+					)}
 				</div>
 				<ProfileHeaderSearchBar onSearchClick={props.onSearchClick} />
 				<img src={props.picture} />
