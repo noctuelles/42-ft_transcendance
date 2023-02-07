213e5398e5b5d1286085b6f7c46ae09cdec3acfb
dhubleur <dhubleur@student.42.fr>
2023-02-07 13:17:55 +0100

    feat(PROFILE): Display user status

--- front/src/components/pages/details/profile/ProfileHeader.tsx
+++ front/src/components/pages/details/profile/ProfileHeader.tsx
@@ -2,21 +2,52 @@ import '@/style/details/profile/ProfileHeader.css';
 import ProfileHeaderSearchBar from './ProfileHeaderSearchBar';
 import ProgressBar from '@/components/global/ProgressBar';
 import getLevelByXP from './Utils';
+import { UserStatus } from './ProfileTypes';
+import { ws_url as WS_URL } from '@/config.json';
+import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
+import { useState } from 'react';
 
 interface ProfileHeaderProps {
+	userId: number;
 	onSearchClick: object;
 	picture: string;
+	status: UserStatus;
 	total_xp: number;
 	username: string;
 }
 
 const ProfileHeader = (props: ProfileHeaderProps) => {
 	let [level, percentCompleted] = getLevelByXP(props.total_xp);
+	const [status, setStatus] = useState(props.status);
+
+	function isStatusChangedEvent(data: any): boolean {
+		return data.event === 'user-status';
+	}
+
+	useWebSocket(WS_URL, {
+		share: true,
+		onMessage: (event) => {
+			const data = JSON.parse(event.data);
+			if (isStatusChangedEvent(data)) {
+				if (data.data.id === props.userId) {
+					setStatus(data.data.status);
+				}
+			}
+		},
+		filter: ({ data }) => {
+			return isStatusChangedEvent(JSON.parse(data));
+		},
+	});
 
 	return (
 		<div className="profile-header">
 			<div className="profile-header-top">
-				<p>{props.username}</p>
+				<div className="profile-header-user">
+					{props.username}{' '}
+					<div className={`profile-status status-${status}`}>
+						{status}
+					</div>
+				</div>
 				<ProfileHeaderSearchBar onSearchClick={props.onSearchClick} />
 				<img src={props.picture} />
 			</div>
