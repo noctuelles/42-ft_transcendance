f32faa4ca02fb9d7080adfb017079a2d2c86fe8a
Jeremy Maia <jmaia@student.42.fr>
2023-02-16 16:40:56 +0100

    Change parameters of game

--- back/src/modules/game/Game.class.ts
+++ back/src/modules/game/Game.class.ts
@@ -275,7 +275,8 @@ export class Game {
 		ball.position.x = this._gameState.gameInfos.width / 2;
 		ball.position.y = this._gameState.gameInfos.height / 2;
 		ball.direction.x = Math.random() * (Math.random() < 0.5 ? -1 : 1);
-		ball.direction.y = (Math.random() / 3) * (Math.random() < 0.5 ? -1 : 1);
+		ball.direction.y =
+			(Math.random() * 0.6 + 0.2) * (Math.random() < 0.5 ? -1 : 1);
 		ball.velocity = GameParams.BALL_DEFAULT_SPEED;
 	}
 
